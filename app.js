b864dcaf8c876abbc86bb2c69a903f0d8fa74575
Rashad Abdul-Salaam <rashad@getskinclick.com>
2014-09-17 23:45:24 -0500

    Initial commit of AngularStore with starting data

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+	var app = angular.module('store', []);
+
+	var gems = [
+    	{ name: 'Azurite', price: 2.95 },
+    	{ name: 'Bloodstone', price: 5.95 },
+    	{ name: 'Zircon', price: 3.95 },
+  	];
+
+	app.controller("StoreController", function(){
+		this.products = gems;
+	  });
+})();
